65b39af52194f8c82dcf5220c0914fc87d4bc265
Pierre <pierre.sjoberg@kau.se>
2021-03-25 11:43:23 +0100

    Slutkod F7

--- F7/script.js
+++ F7/script.js
@@ -1,1 +1,43 @@
 'use strict';
+
+window.addEventListener('load', prepareColorPicker);
+
+function prepareColorPicker() {
+   
+    let fields = document.querySelectorAll('.field');
+
+    for(let i = 0; i < fields.length; i++) {
+
+        let input = fields[i].querySelector('input');
+        let span = fields[i].querySelector('span');
+
+        input.value = Math.floor(Math.random()*256);
+        span.textContent = input.value;
+
+        fields[i].addEventListener('input', handleValueChange);
+
+    }
+
+    updateBackground();
+
+}
+
+function updateBackground() {
+
+    let red = document.querySelector('#red').value;
+    let green = document.querySelector('#green').value;
+    let blue = document.querySelector('#blue').value;
+
+    let newColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    //document.querySelector('body').style.backgroundColor = newColor;
+    document.querySelector('body').setAttribute('style','background-color: ' + newColor);
+}
+
+function handleValueChange(oEvt) {
+
+    let span = oEvt.currentTarget.querySelector('.value');
+    span.textContent = oEvt.target.value;
+    
+    updateBackground();
+
+}
