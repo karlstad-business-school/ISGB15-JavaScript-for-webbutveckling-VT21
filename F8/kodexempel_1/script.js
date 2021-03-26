a91e08b7bf86554e98c6affead595a72becec9d2
Pierre <pierre.sjoberg@kau.se>
2021-03-26 12:06:49 +0100

    F8 slutkod

--- F8/kodexempel_1/script.js
+++ F8/kodexempel_1/script.js
@@ -1,2 +1,67 @@
 'use strict';
 
+window.addEventListener('load', ()=>{
+    //renderPage();
+    //renderPage2();
+    renderPage3();
+});
+
+function renderPage() {
+
+    document.writeln('<main>');
+    document.writeln('<h1>');
+    document.writeln('En rubrik...');
+    document.writeln('</h1>');
+    document.writeln('<button class="btn btn-primary">Push!</button>');
+    document.writeln('</main>');
+}
+
+function renderPage2() {
+    let body = document.querySelector('body');
+    body.innerHTML = "<main><h1>En rubrik</h1><button class='btn btn-primary'>Push!</button></main>";
+  
+}
+
+function renderPage3() {
+    let body = document.querySelector('body');
+    let mainElement = document.createElement('main');
+    let rubrik = document.createElement('h1');
+    let textNode = document.createTextNode('En rubrik...');
+    let knapp = document.createElement('button');
+    knapp.classList.add('btn','btn-primary');
+
+    body.appendChild(mainElement);
+    mainElement.appendChild(rubrik);
+    rubrik.appendChild(textNode);
+    textNode = document.createTextNode('Push!');
+    mainElement.appendChild(knapp);
+    knapp.appendChild(textNode);
+
+    knapp.addEventListener('click', btnClick);
+}
+
+function btnClick(oEvt) {
+
+    let slmp = Math.floor((Math.random() * 5) +1);
+    console.log(slmp);
+    switch(slmp) {
+        case 1:
+            this.setAttribute('class','btn btn-primary');
+            break;
+        case 2: 
+            this.setAttribute('class', 'btn btn-danger');
+            break;
+        case 3:
+            this.setAttribute('class', 'btn btn-warning');
+            break;
+        case 4:
+            this.setAttribute('class', 'btn btn-secondary');
+            break;
+        case 5:
+            let newButton = document.createElement('button');
+            let text = document.createTextNode('Push!');
+            newButton.appendChild(text);
+            newButton.addEventListener('click', btnClick);
+            document.querySelector('main').insertBefore(newButton, this);
+    }
+}
--- F8/kodexempel_2/script.js
+++ F8/kodexempel_2/script.js
@@ -1,3 +1,37 @@
 'use strict';
 
+window.addEventListener('load', ()=> {
+    renderPage();
+    document.querySelector('body').addEventListener('click', bodyClick);
+});
 
+function renderPage() {
+    let table = document.querySelector('table');
+    let counter = 1;
+
+    for(let rad = 1; rad <=10; rad++) {
+
+        let tr = document.createElement('tr');
+
+        for(let kol=1; kol<=10; kol++) {
+            let td = document.createElement('td');
+            td.classList.add('p-3');
+            let text = document.createTextNode(counter);
+            counter++;
+
+            td.appendChild(text);
+            tr.appendChild(td);
+        }
+
+        table.appendChild(tr);
+
+    }
+
+}
+
+function bodyClick(oEvt) {
+
+    console.log(oEvt.target.tagName);
+    oEvt.target.style.backgroundColor='red';
+
+}
